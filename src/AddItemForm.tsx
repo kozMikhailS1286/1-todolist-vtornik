9bb1b93b11df7b3c442a91cf6095b857a4938957
Mikhail <koz.mikhail.s@yandex.ru>
2023-01-16 21:40:34 +0300

    Tuesday 2 week / 1

---
+++ src/AddItemForm.tsx
@@ -0,0 +1,46 @@
+import React, {useState, ChangeEvent, KeyboardEvent} from 'react';
+import './App.css';
+
+
+type PropsType = {
+    callBack: (title: string) => void
+}
+
+export const AddItemForm = (props: PropsType) => {
+
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+
+    const addTask = () => {
+        let newTitle = title.trim();
+        if (newTitle !== "") {
+            props.callBack(newTitle);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.charCode === 13) {
+            addTask();
+        }
+    }
+
+    return (
+        <div>
+            <input value={title}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}
+                   className={error ? "error" : ""}
+            />
+            <button onClick={addTask}>+</button>
+            {error && <div className="error-message">{error}</div>}
+        </div>
+    )
+};
